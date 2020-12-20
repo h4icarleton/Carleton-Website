b4b47d03e5d8218b90d4f2e2c3bb586dff13110d
Maya Murphy <murphym2@carleton.edu>
2020-12-20 08:16:32 -0600

    added sections for dev/design/code teams

--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -5,6 +5,10 @@ import MemberProfileContainer from '../components/MemberProfileContainer'
 import './pages.css';
 import BigProfiles from '../components/BigProfiles';
 
+// const DevTeamArray = [];
+// const DevTeamArray = [];
+// const DevTeamArray = [];
+
 const OurTeam = () => {
   return(
     <div className="background" style={{flexDirection:'column', overflowY:'scroll', overflowX:'hidden'}}>
@@ -14,58 +18,76 @@ const OurTeam = () => {
       <div style={{marginTop:'200px', marginBottom:'200px', alignItems:'center', display:'flex', justifyContent:'space-bewteen', flexDirection:'column'}}>
         <MemberProfileContainer>
           {
-            <MemberProfile 
-              fullName='PJ Sangvong' 
-              position='Developer?' 
-              classYear='2023' 
-              imgPath='/images/profilePictures/PJProfile.jpg' 
-              email='sangvongp@carleton.edu' 
+            <MemberProfile
+              fullName='PJ Sangvong'
+              position='Developer?'
+              classYear='2023'
+              imgPath='/images/profilePictures/PJProfile.jpg'
+              email='sangvongp@carleton.edu'
               linkedinProfileUrl='https://www.linkedin.com/in/sangvongp'
             />
           }
           {
-            <MemberProfile 
-              fullName='Dummy A' 
-              position='being a dummy' 
-              classYear='0000' 
-              imgPath='/images/profilePictures/dummy-profile.jpeg' 
-              email='dummy@gmail.com' 
+            <MemberProfile
+              fullName='Dummy A'
+              position='being a dummy'
+              classYear='0000'
+              imgPath='/images/profilePictures/dummy-profile.jpeg'
+              email='dummy@gmail.com'
               linkedinProfileUrl='https://www.linkedin.com/school/carleton-college/'
             />
           }
           {
-            <MemberProfile 
-              fullName='Dummy B' 
-              position='who uses email?' 
-              classYear='0000' 
-              imgPath='/images/profilePictures/dummy-profile.jpeg' 
+            <MemberProfile
+              fullName='Dummy B'
+              position='who uses email?'
+              classYear='0000'
+              imgPath='/images/profilePictures/dummy-profile.jpeg'
               linkedinProfileUrl='https://www.linkedin.com/school/carleton-college/'
             />
           }
           {
-            <MemberProfile 
-              fullName='Dummy C' 
-              position='what is linkedin?' 
-              classYear='0000' 
-              imgPath='/images/profilePictures/dummy-profile.jpeg' 
-              email='dummy@gmail.com' 
+            <MemberProfile
+              fullName='Dummy C'
+              position='what is linkedin?'
+              classYear='0000'
+              imgPath='/images/profilePictures/dummy-profile.jpeg'
+              email='dummy@gmail.com'
             />
           }
         </MemberProfileContainer>
         <MemberProfileContainer>
           {
-            <MemberProfile 
-              fullName='Dummy D' 
-              position='new container' 
-              classYear='0000' 
-              imgPath='/images/profilePictures/dummy-profile.jpeg' 
-              email='dummy@gmail.com' 
+            <MemberProfile
+              fullName='Dummy D'
+              position='new container'
+              classYear='0000'
+              imgPath='/images/profilePictures/dummy-profile.jpeg'
+              email='dummy@gmail.com'
             />
           }
         </MemberProfileContainer>
+
+        <MemberProfileContainer>
+          <div className="DevTeam" style={{color: '#FFFFFF'}}>
+            <h1> Development </h1>
+          </div>
+        </MemberProfileContainer>
+
+        <MemberProfileContainer>
+          <div className="Design-UserResearchTeam" style={{color: '#FFFFFF'}}>
+            <h1> Design && User Research </h1>
+          </div>
+        </MemberProfileContainer>
+
+        <MemberProfileContainer>
+          <div className="CodeReview-TestingTeam" style={{color: '#FFFFFF'}}>
+            <h1> Code Review && Testing </h1>
+          </div>
+        </MemberProfileContainer>
       </div>
     </div>);
-  
+
 }
 
 export default OurTeam;
