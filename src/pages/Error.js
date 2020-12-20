b271540b7e3a60868ae784a82fa611e2b78e5702
Etienne Richart <etiennerichart@gmail.com>
2020-12-20 13:01:46 -0500

    worked on error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './pages.css';
+import "../components/HeaderTypingAnimation.css";
 
 const Error = () => {
     return (
-      <div className="background">
-          <div className="container">
-            <h1><strong>Error:</strong> This is not a valid page.</h1>
+      <div className="header-wrapper">
+          <div className="containerf" style={{paddingTop: '40px', marginRight: '0'}}>
+            <h1 style={{marginBottom: '20px'}}><strong>Error:</strong> This is not a valid page.</h1>
             <Link className='butn' to="/" style={{textDecoration : "none", color: "rgba(238, 177, 17, 1)" }}>Return Home</Link>
           </div>
           
