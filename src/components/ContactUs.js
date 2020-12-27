ac116e1598d1f6f7414c9279881b723efae3b16a
shawqin <62914323+shawqin@users.noreply.github.com>
2020-12-27 07:02:58 -0600

    attempt to stick footer to bottom of page

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactUs = () => {
     var emailLink = <a href="mailto:carleton@hack4impact.org" target="_blank" rel="noopener noreferrer">carleton@hack4impact.org</a>;
     return (
-        <footer class="contact-us-footer" style={{position: "fixed", bottom: 0, left: 0, height: "30px", width: "100%", padding: "0.3rem", backgroundColor: "rgba(255, 255, 255, 0.5)", fontSize: "18px"}}>
+        <footer class="contact-us-footer" style={{height: "30px", width: "100%", padding: "0.3rem", backgroundColor: "rgba(255, 255, 255, 0.5)", fontSize: "18px"}}>
             <p>Any question about Hack4Impact@Carleton? <span style={{fontWeight: "bold"}}>Contact us:</span> {emailLink}</p>
         </footer>
     )
