616aa5b7f0a5eb4fcaf9e49d7da8776328c19e06
Maya Murphy <murphym2@carleton.edu>
2020-12-27 07:57:45 -0600

    added arrows for dropdown component

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,17 +1,18 @@
 import Members from '../components/Members.js';
 import React, {Component} from 'react';
-import { MDBIcon } from 'mdbreact';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 /**
  *
- * @param props {team}
+ * @param props {team, key}
  */
   // fullName, position, classYear, imgPath, email, linkedinProfileUrl
 const Dropdown = (props) => {
   return (
     <div className="dd-wrapper">
       <h1 style={{ color : 'white', fontSize : '50px'}}> {props.team} </h1>
-
+      <span> <i class="fas fa-angle-down" style={{ color : 'white', fontSize : '50px', alignItems:'right'}}></i> </span>
     <div className="dd-list">
 
     </div>
