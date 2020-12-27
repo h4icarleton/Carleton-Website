509615090c6f7c63caba03ca5b49cf4d83f8676a
Etienne Richart <etiennerichart@gmail.com>
2020-12-27 10:50:03 -0500

    Added alt to img and remove duplicate css thing

--- src/components/MemberProfile.js
+++ src/components/MemberProfile.js
@@ -17,7 +17,7 @@ const LinkText = ({text, email}) => (
 const MemberProfile = (props) => {
     return <div style={{ width:'350px', height:'200px' ,display:'flex', flexDirection:'row', justifyContent:'space-between', padding:'30px'}}>
         <div style={{width:'130px'}}>
-            <img src={`${process.env.PUBLIC_URL}${props.imgPath}`} style={{width:'100%', clipPath:'circle(50% at 50% 50%)'}} />
+            <img src={`${process.env.PUBLIC_URL}${props.imgPath}`} style={{width:'100%', clipPath:'circle(50% at 50% 50%)'}} alt="Progile Image" />
         </div>
         <div style={{display:"flex", justifyContent:'center', flexDirection:'column'}}>
             <NormalText normalText={`${props.fullName}`}/>
