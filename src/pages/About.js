dc6638e7cd7c3b600c17237685a0d7baefb329cb
Victor <huangv2@carleton.edu>
2021-01-05 01:26:37 -0500

    second version of about page, fixed padding issue, problem with font still exists

--- src/pages/About.js
+++ src/pages/About.js
@@ -11,8 +11,8 @@ const About = () => {
   return (
     <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
       <div className = "flexing">
-        <div className = "containerf startf" style = {{display: 'flex',  justifyContent: 'center', alignItems: 'center', width: '50%', margin: 'auto'}}>
-              <p style={{fontFamily: 'Arial', fontSize: '23px', fontStyle: 'normal', fontWeight: '400', lineHeight: '26px', letterSpacing: '0em', textAlign: 'center'}}>
+        <div className = "containerf startf" style = {{display: 'flex',  justifyContent: 'center', alignItems: 'center', width: '95%', margin: 'auto', height: '20%'}}>
+            <p style={{paddingLeft: '47px', paddingRight: '47px', paddingTop: '15px', paddingBottom: '15px', fontFamily: 'Arial', fontSize: '23px', fontStyle: 'normal', fontWeight: '400', lineHeight: '26px', letterSpacing: '0em', textAlign: 'center'}}>
               Technology is changing the world, one line of code at a time. Students at Carleton recognize that nonprofits have their hands tied-- they’re invested in creating social impact. We’re here to help them realize their vision and create a tangible, technological solution that solves their problems.
               </p>
           <button onClick = {handleClick}>Flip</button>
@@ -21,8 +21,8 @@ const About = () => {
  
     <div className = "flexing">
         <div className = "containerf startf">
-            <div style = {{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
-            <p>
+        <div className = "containerf startf" style = {{display: 'flex',  justifyContent: 'center', alignItems: 'center', width: '95%', margin: 'auto', height: '20%'}}>
+            <p style={{paddingLeft: '47px', paddingRight: '47px', paddingTop: '15px', paddingBottom: '15px', fontFamily: 'Arial', fontSize: '23px', fontStyle: 'normal', fontWeight: '400', lineHeight: '26px', letterSpacing: '0em', textAlign: 'center'}}>
               Technology is changing the world, one line of code at a time. Students at Carleton recognize that nonprofits have their hands tied-- they’re invested in creating social impact. We’re here to help them realize their vision and create a tangible, technological solution that solves their problems.
               </p>
             <button onClick={handleClick}>Flip</button>
