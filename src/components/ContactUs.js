6c4f9a5d4f1e2e676b57472d84f6500e0b826765
Etienne Richart <etiennerichart@gmail.com>
2021-01-06 23:10:39 -0500

    Made contact us banner more responsive

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './contact.css';
 
 const ContactUs = () => {
     var emailLink = <a href="mailto:carleton@hack4impact.org" target="_blank" rel="noopener noreferrer">carleton@hack4impact.org</a>;
     return (
-        <footer className="contact-us-footer" style={{position: "fixed", bottom: 0, left: 0, height: "30px", width: "100%", padding: "0.3rem", backgroundColor: "rgba(255, 255, 255, 0.5)", fontSize: "18px"}}>
+        <footer className="contact-us-footer">
             <p>Any question about Hack4Impact@Carleton? <span style={{fontWeight: "bold"}}>Contact us:</span> {emailLink}</p>
         </footer>
     )
