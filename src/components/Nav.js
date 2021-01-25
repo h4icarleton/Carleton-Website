5c9d74ecc16c5f7900f1dc9877cfdb70d1217578
Etienne Richart <etiennerichart@gmail.com>
2021-01-25 02:10:06 -0600

    Work on navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
 
 const Nav = () => {
-
-    const location = useLocation();
+    let location = useLocation();
     const [curLocation, setLocation] = useState({
         location: location.pathname
     });
@@ -13,17 +12,54 @@ const Nav = () => {
         setLocation(location.pathname);
     }, [location.pathname]);
 
-    
+    const [display, setDisplay] = useState(false);
+
+    function displayNav() {
+        if (display) {
+            setDisplay(false);
+        } else {
+            setDisplay(true);
+        }
+    }
+
+        // Initialize state with undefined width/height so server and client renders match
+        // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+        const [windowSize, setWindowSize] = useState({
+          width: undefined,
+          height: undefined,
+        });
+      
+        useEffect(() => {
+          // Handler to call on window resize
+          function handleResize() {
+            // Set window width/height to state
+            setWindowSize({
+              width: window.innerWidth,
+              height: window.innerHeight,
+            });
+          }
+          
+          // Add event listener
+          window.addEventListener("resize", handleResize);
+          
+          // Call handler right away so state gets updated with initial window size
+          handleResize();
+
+        }, []);
 
   return (
     <>
     <nav className="navbar navbar-expand-sm navbar-light" style={{position: "fixed", top: 0, left: 0, zIndex : 1, width: "100%", backgroundColor: "rgba(255, 255, 255, 0.5)"}}>
         <Link className="navbar-brand" style={{padding: '8px'}} to="/">
-            <img id="h4iLogo" src="https://hack4impact.org/svg/logo.svg" alt="Hack4Impact Logo" />
+            <div id="h4i"><img id="h4iLogo" src="https://hack4impact.org/svg/logo.svg" alt="Hack4Impact Logo" /></div>
             <img id="ccLogo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Carleton_College_logo.svg/1280px-Carleton_College_logo.svg.png" alt="Carleton College"/>
         </Link>
-
-        <ul className="navbar-nav" >
+        <span className="toggle-icon" onClick={displayNav}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+        </span>
+        <ul className="navbar-nav" style={ windowSize.width > 650 || display ? {display: 'flex'} : {display: 'none'}}>
             <li className="nav-item">
                 <Link className="nav-link" to="/"  style={ curLocation === '/' ? {fontWeight: 'bold'} : { fontWeight: 'normal' }}>home</Link>
             </li>
