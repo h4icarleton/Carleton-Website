7d115751f4059b89fdd8ff1475df5778fa568fe5
Etienne Richart <etiennerichart@gmail.com>
2021-02-16 22:53:06 -0600

    fix the error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './pages.css';
-import "../components/HeaderTypingAnimation.css";
+import '../components/HeaderTypingAnimation.css';
 
 const Error = () => {
     return (
-      <div className="header-wrapper">
+      <div className="background-image header-wrapper">
           <div className="containerf" style={{paddingTop: '40px', marginRight: '0'}}>
             <h1 style={{marginBottom: '20px'}}><strong>Error:</strong> This is not a valid page.</h1>
-            <Link className='butn' to="/" style={{textDecoration : "none", color: "rgba(238, 177, 17, 1)" }}>Return Home</Link>
+            <Link className='butn aft-btn' to="/" style={{textDecoration : "none", color: "rgba(238, 177, 17, 1)" }}>Return Home</Link>
           </div>
-          
       </div>
     )
   }
