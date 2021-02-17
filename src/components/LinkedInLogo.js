39a76a165823438b27bf64cb1e46473492e23402
Etienne Richart <etiennerichart@gmail.com>
2021-02-16 23:42:03 -0600

    remove console log

--- src/components/LinkedInLogo.js
+++ src/components/LinkedInLogo.js
@@ -1,7 +1,6 @@
 import './LinkedInLogo.css';
 
 const LinkedInLogo = (props) => {
-    console.log(props.linkedInUrl)
     return <div>
         <a href={props.linkedInUrl} target="_blank" rel="noreferrer">
             <img src={ `${process.env.PUBLIC_URL}/images/logo/linkedInLogo.png`} style={{width:'20px'}} className='logo' alt="logo" />
