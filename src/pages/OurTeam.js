748a902200ee2182dea8a4bd501b2bf21b73d1d7
Maya Murphy <murphym2@carleton.edu>
2021-08-02 12:42:45 -0500

    feat: added new category: Social Media Team to Our Team page

--- src/pages/OurTeam.js
+++ src/pages/OurTeam.js
@@ -1,8 +1,8 @@
-import React, {useRef, useLayoutEffect, useState} from 'react';
-import SliderContainer from '../components/SliderContainer'
-import './pages.css';
-import BigProfiles from '../components/BigProfiles';
-import Dropdown from '../components/Dropdown.js'
+import React, { useRef, useLayoutEffect, useState } from "react";
+import SliderContainer from "../components/SliderContainer";
+import "./pages.css";
+import BigProfiles from "../components/BigProfiles";
+import Dropdown from "../components/Dropdown.js";
 
 const OurTeam = () => {
   const div = useRef();
@@ -13,22 +13,67 @@ const OurTeam = () => {
     setWidth(div.current.getBoundingClientRect().width);
     setInWidth(indiv.current.getBoundingClientRect().width);
   }, []);
-  
-  return(
+
+  return (
     <>
-    <div className = "our-team-background"></div>
-    <div id="ourteam-scroll" style={{flexDirection:'column', overflowY:'scroll', marginTop:'12vh'}}>
-      <div style={{ width:'100%', marginBottom:'auto', overflowX: 'hidden'}}>
-        <SliderContainer slides={BigProfiles} refA={div} refB={indiv} autoPlay={3.5} width={width} indiv={indiv}/>
+      <div className="our-team-background"></div>
+      <div
+        id="ourteam-scroll"
+        style={{
+          flexDirection: "column",
+          overflowY: "scroll",
+          marginTop: "12vh",
+        }}
+      >
+        <div
+          style={{ width: "100%", marginBottom: "auto", overflowX: "hidden" }}
+        >
+          <SliderContainer
+            slides={BigProfiles}
+            refA={div}
+            refB={indiv}
+            autoPlay={3.5}
+            width={width}
+            indiv={indiv}
+          />
+        </div>
+        <div
+          ref={div}
+          style={{
+            marginTop: "40px",
+            marginBottom: "60px",
+            alignItems: "center",
+            display: "flex",
+            justifyContent: "space-bewteen",
+            flexDirection: "column",
+          }}
+        >
+          <Dropdown
+            team="Directors & Leadership"
+            shortTeam="Directors"
+            teamIndex="Directors"
+            refer={indiv}
+          />
+          <Dropdown team="Development Team" shortTeam="Dev" teamIndex="Dev" />
+          <Dropdown
+            team="Design & User Research Team"
+            shortTeam="Design-User"
+            teamIndex="Design-User"
+          />
+          <Dropdown
+            team="Code Review & Testing Team"
+            shortTeam="Code-Testing"
+            teamIndex="Code-Testing"
+          />
+          <Dropdown
+            team="Social Media & Marketing Team"
+            shortTeam="Social-Marketing"
+            teamIndex="Social-Marketing"
+          />
+        </div>
       </div>
-      <div ref={div} style={{marginTop:'40px', marginBottom:'60px', alignItems:'center', display:'flex', justifyContent:'space-bewteen', flexDirection:'column'}}>
-        <Dropdown team="Directors & Leadership" shortTeam ="Directors" teamIndex="Directors" refer={indiv}/>
-        <Dropdown team="Development Team" shortTeam = 'Dev' teamIndex="Dev"/>
-        <Dropdown team="Design & User Research Team" shortTeam = 'Design-User' teamIndex="Design-User"/>
-        <Dropdown team="Code Review & Testing Team" shortTeam = 'Code-Testing' teamIndex="Code-Testing"/>
-      </div>
-    </div>
-    </>);
-} 
+    </>
+  );
+};
 
 export default OurTeam;
