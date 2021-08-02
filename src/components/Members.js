378aa764c6af0dc6cf114567462acab2e83d8d96
Maya Murphy <murphym2@carleton.edu>
2021-08-02 12:41:27 -0500

    fix: updated members to reflect current membership

--- src/components/Members.js
+++ src/components/Members.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import MemberProfile from '../components/MemberProfile';
-import MemberProfileContainer from '../components/MemberProfileContainer';
-import '../components/Slides.css'
+import React from "react";
+import MemberProfile from "../components/MemberProfile";
+import MemberProfileContainer from "../components/MemberProfileContainer";
+import "../components/Slides.css";
 
 /**
  *
@@ -10,289 +10,100 @@ import '../components/Slides.css'
 // want to return MemberProfile
 const Members = (props) => {
   const isListOpen = props.status;
-  const isMemberArray = []
+  const isMemberArray = [];
   members.map((person) => {
-      if (props.shortTeam === person.team){
-        isMemberArray.push(true)
-      } else {
-        isMemberArray.push(false)
-      }
-  })
-
-    return (<MemberProfileContainer>
-    {members.map((person, index) => {
-        return isListOpen && isMemberArray[index] &&
-        <MemberProfile key={person.fullName} fullName={person.fullName} position={person.position} classYear={person.classYear} imgPath={person.imgPath} email={person.email} linkedinProfileUrl={person.linkedinProfileUrl} />
+    if (props.shortTeam === person.team) {
+      isMemberArray.push(true);
+    } else {
+      isMemberArray.push(false);
     }
-    )}
-    </MemberProfileContainer>)
-      
-}
+  });
 
+  return (
+    <MemberProfileContainer>
+      {members.map((person, index) => {
+        return (
+          isListOpen &&
+          isMemberArray[index] && (
+            <MemberProfile
+              key={person.fullName}
+              fullName={person.fullName}
+              position={person.position}
+              classYear={person.classYear}
+              imgPath={person.imgPath}
+              email={person.email}
+              linkedinProfileUrl={person.linkedinProfileUrl}
+            />
+          )
+        );
+      })}
+    </MemberProfileContainer>
+  );
+};
 
 const members = [
-  
   {
-    fullName:'Aishwarya Varma',
-    position:'Executive Director and CoFounder',
-    team:'Directors',
-    classYear:'2023',
-    imgPath:'/images/profilePictures/dummy-profile.jpeg',
-    email:'varmaa@carleton.edu',
-    linkedinProfileUrl:'https://www.linkedin.com/in/aishwaryavarma01/',
+    fullName: "Aishwarya Varma",
+    position: "Executive Director and CoFounder",
+    team: "Directors",
+    classYear: "2023",
+    imgPath: "/images/profilePictures/AishwaryaProfile.jpg",
+    email: "varmaa@carleton.edu",
+    linkedinProfileUrl: "https://www.linkedin.com/in/aishwaryavarma01/",
   },
   {
-    fullName:'Nicole Binder',
-    position:'Executive Director and CoFounder',
-    team:'Directors',
-    classYear:'2021',
-    imgPath:'/images/profilePictures/dummy-profile.jpeg',
-    email:'bindern@carleton.edu',
-    linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/',
-},
-  {
-    fullName:'PJ Sangvong',
-    position:'Developer',
-    team:'Dev',
-    classYear:'2023',
-    imgPath:'/images/profilePictures/PJProfile.jpg',
-    email:'sangvongp@carleton.edu',
-    linkedinProfileUrl:'https://www.linkedin.com/in/sangvongp',
+    fullName: "PJ Sangvong",
+    position: "Developer",
+    team: "Dev",
+    classYear: "2023",
+    imgPath: "/images/profilePictures/PJProfile.jpg",
+    email: "sangvongp@carleton.edu",
+    linkedinProfileUrl: "https://www.linkedin.com/in/sangvongp",
   },
   {
-      fullName:'Henrie Friesen',
-      positon:'Developer',
-      team:'Dev',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'fiesenh@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/',
+    fullName: "Maya Murphy",
+    position: "Developer",
+    team: "Dev",
+    classYear: "2022",
+    imgPath: "/images/profilePictures/MayaProfile.png",
+    email: "murphym2@carleton.edu",
+    linkedinProfileUrl: "https://www.linkedin.com/in/maya-and-tech",
   },
   {
-
-      fullName:'Zhixing Jiang',
-      positon:'Developer',
-      team:'Dev',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'jiangz@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
+    fullName: "Etienne Richart",
+    position: "Developer",
+    team: "Dev",
+    classYear: "2023",
+    imgPath: "/images/profilePictures/EtienneProfile.jpg",
+    email: "richarte@carleton.edu",
+    linkedinProfileUrl: "https://www.linkedin.com/in/etiennerichart/",
   },
   {
-
-      fullName:'Juanito Zhang Yang',
-      positon:'Developer',
-      team:'Dev',
-      classYear:'2022',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'zhangyangj@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
+    fullName: "PJ Sangvong",
+    position: "UI Designer",
+    team: "Design-User",
+    classYear: "2023",
+    imgPath: "/images/profilePictures/PJProfile.jpg",
+    email: "sangvongp@carleton.edu",
+    linkedinProfileUrl: "https://www.linkedin.com/in/sangvongp",
   },
   {
-      fullName:'Quoc Nguyen',
-      positon:'Developer',
-      team:'Dev',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'nguyenq2@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
+    fullName: "Maya Murphy",
+    position: "Content Creator",
+    team: "Social-Marketing",
+    classYear: "2022",
+    imgPath: "/images/profilePictures/MayaProfile.png",
+    email: "murphym2@carleton.edu",
+    linkedinProfileUrl: "https://www.linkedin.com/in/maya-and-tech",
   },
   {
-      fullName:'Kevin Chen',
-      positon:'Developer',
-      team:'Dev',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'chenk4@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
+    fullName: "Aishwarya Varma",
+    positon: "Co-Founder",
+    team: "Admin",
+    classYear: "2023",
+    imgPath: "/images/profilePictures/dummy-profile.jpeg",
+    email: "varmaa@carleton.edu",
+    linkedinProfileUrl: "https://www.linkedin.com/school/carleton-college/",
   },
-  {
-      fullName:'Etienne Richart',
-      positon:'Developer',
-      team:'Dev',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'richarte@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/in/etiennerichart/'
-  },
-  {
-
-      fullName:'Tessa Newman-Heggie',
-      positon:'Project Lead of Design',
-      team:'Design-User',
-      classYear:'2021',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'newmant2@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Lydia Fick',
-      positon:'UI Designer',
-      team:'Design-User',
-      classYear:'2022',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'fickl@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Emily Litton',
-      positon:'UI Designer',
-      team:'Design-User',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'littone@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'James Marlin',
-      positon:'UI Designer',
-      team:'Design-User',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'marlinj@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'William Dudarov',
-      positon:'UI Designer',
-      team:'Design-User',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'dudarovw@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/',
-
-  },
-  {
-
-      fullName:'Starr Wang',
-      positon:'UI Designer',
-      team:'Design-User',
-      classYear:'2021',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'wangy3@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'PJ Sangvong',
-      position:'UI Designer',
-      team:'Design',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/PJProfile.jpg',
-      email:'sangvongp@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/in/sangvongp'
-
-  },
-  {
-
-      fullName:'Eric Odoom',
-      positon:'UX Designer',
-      team:'Design-User',
-      classYear:'2021',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'odoome@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/',
-
-  },
-  {
-
-      fullName:'Eliot Huh',
-      positon:'UX Designer',
-      team:'Design-User',
-      classYear:'2021',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'huhe@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Victor Huang',
-      positon:'Project Lead of Code Review',
-      team:'Code-Testing',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'huangv2@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Shaw Qin',
-      positon:'Code Reviewer',
-      team:'Code-Testing',
-      classYear:'2022',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'qins@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Yasmeen Awad',
-      positon:'Project Lead of Testing',
-      team:'Code-Testing',
-      classYear:'2021',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'awady@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Ankit Sanghi',
-      positon:'Code Tester',
-      team:'Code-Testing',
-      classYear:'2021',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'sanghia@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Lita Theng',
-      positon:'Code Tester',
-      team:'Code-Testing',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'thengl@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Aishwarya Varma',
-      positon:'Co-Founder',
-      team:'Admin',
-      classYear:'2023',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'varmaa@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  },
-  {
-
-      fullName:'Nicole Binder',
-      positon:'Co-Founder',
-      team:'Admin',
-      classYear:'2021',
-      imgPath:'/images/profilePictures/dummy-profile.jpeg',
-      email:'bindern@carleton.edu',
-      linkedinProfileUrl:'https://www.linkedin.com/school/carleton-college/'
-
-  }
 ];
 export default Members;
