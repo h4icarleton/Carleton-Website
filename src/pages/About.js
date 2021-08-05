2d8f7064724cb54368bef83a7a6342b686231b47
Maya Murphy <murphym2@carleton.edu>
2021-08-05 14:38:18 -0500

    update: re-worded values and padding for small screens

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,71 +1,110 @@
-import React, { useState } from 'react';
-import './about.css';
-import './pages.css';
+import React, { useState } from "react";
+import "./about.css";
+import "./pages.css";
 
 const About = () => {
-    const [clickedA, setClickA] = useState(false);
-    const [clickedB, setClickB] = useState(false);
-    const [clickedC, setClickC] = useState(false);
+  const [clickedA, setClickA] = useState(false);
+  const [clickedB, setClickB] = useState(false);
+  const [clickedC, setClickC] = useState(false);
   return (
     <>
-        <div className="background-container">
+      <div className="background-container">
         <div className="about-background"></div>
         <div className="heading-container about">
-            <span className="our-values">Our Values</span><div className="abScroll" id="about-scroll"></div>
+          <span className="our-values">Our Values</span>
+          <div className="abScroll" id="about-scroll"></div>
         </div>
-        <div className = "flexingl">
-            <div className="containerl">
-                <div className='l-container'>
-                    <p><span className = "t-bold">Technology is changing the world, one line of code at a time. </span>Students at Carleton recognize that nonprofits have their hands tied-- they’re invested in creating social impact. We’re here to help them realize their vision and create a tangible, technological solution that solves their problems.</p>
-                </div>
+        <div className="flexingl">
+          <div className="containerl">
+            <div className="l-container">
+              <p>
+                <span className="t-bold">
+                  Technology is changing the world, one line of code at a time.{" "}
+                </span>
+                Students at Carleton recognize that nonprofits have their hands
+                tied-- they’re invested in creating social impact. We’re here to
+                help them realize their vision and create a tangible,
+                technological solution that solves their problems.
+              </p>
             </div>
+          </div>
         </div>
-        </div>  
-        <div className = "flexingl cards">
-            <div className="flip-card" onClick={() => setClickA(!clickedA)}>
-                <div className="flip-card-inner" style={ clickedA ? {transform: 'rotateY(180deg)'} : {}}>
-                    <div className="flip-card-front">
-                        <div className="butnl">
-                            diligence
-                        </div>
-                    </div>
-                    <div className="flip-card-back">
-                        <p> Carleton is a liberal arts college; unlike other schools, it focuses on providing its students (Carls) with a personalized,
-              well-rounded education. Therefore, Carls are unique. Here at Hack4Impact Carleton we focus on using our diverse skillset to 
-              create innovative technological solutions that help our community.
-                        </p>
-                    </div>
-                </div>
+      </div>
+      <div className="flexingl cards">
+        <div className="flip-card" onClick={() => setClickA(!clickedA)}>
+          <div
+            className="flip-card-inner"
+            style={clickedA ? { transform: "rotateY(180deg)" } : {}}
+          >
+            <div className="flip-card-front">
+              <div className="butnl">diligence</div>
             </div>
-            <div className="flip-card" onClick={() => setClickB(!clickedB)}>
-                <div className="flip-card-inner" style={ clickedB ? {transform: 'rotateY(180deg)'} : {}}>
-                    <div className="flip-card-front">
-                        <div className="butnl">
-                            diversity
-                        </div>
-                    </div>
-                    <div className="flip-card-back">
-                        <p>Carls are not only diligent, but also politically progressive and socially active. At H4I Carleton, we’re committed to promoting diversity in CS. People of color and women are already underrepresented in the field, thus it is our mission to diversify the narrative by providing opportunities for these communities to get real-world software experience.
-
-                        </p>
-                    </div>
-                </div>
+            <div className="flip-card-back">
+              <p
+                style={{
+                  paddingLeft: "10px",
+                  paddingRight: "10px",
+                  textAlign: "justify",
+                }}
+              >
+                {" "}
+                We believe that having a steady, earnest, and energetic effort
+                to creatively solve a problem transforms each member in our
+                chapter and prepares them for a rapidly changing world.{" "}
+              </p>
             </div>
-            <div className="flip-card" onClick={() => setClickC(!clickedC)}>
-                <div className="flip-card-inner" style={ clickedC ? {transform: 'rotateY(180deg)'} : {}}>
-                    <div className="flip-card-front">
-                        <div className="butnl">
-                            collaboration
-                        </div>
-                    </div>
-                    <div className="flip-card-back">
-                        <p>Carleton is a small college with only 1,500 students. In other words, we tend to be extremely close-knit. At a time like this, social connection is increasingly difficult, making it hard to maintain solid relationships. However, this won’t deter us-- although we’re working remotely, our team is persistent, engaged, and community-driven.
-
-                        </p>
-                    </div>
-                </div>
+          </div>
+        </div>
+        <div className="flip-card" onClick={() => setClickB(!clickedB)}>
+          <div
+            className="flip-card-inner"
+            style={clickedB ? { transform: "rotateY(180deg)" } : {}}
+          >
+            <div className="flip-card-front">
+              <div className="butnl">diversity</div>
             </div>
-        </div>  
+            <div className="flip-card-back">
+              <p
+                style={{
+                  paddingLeft: "12px",
+                  paddingRight: "12px",
+                  textAlign: "justify",
+                }}
+              >
+                We believe that making space for diverse ability, culture,
+                identity, and opinion makes our team stronger. Doing so equips
+                each team member with the power to meaningfully encounter
+                difference, live fruitfully in society, and contribute
+                effectively to work they pursue here and beyond.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flip-card" onClick={() => setClickC(!clickedC)}>
+          <div
+            className="flip-card-inner"
+            style={clickedC ? { transform: "rotateY(180deg)" } : {}}
+          >
+            <div className="flip-card-front">
+              <div className="butnl">collaboration</div>
+            </div>
+            <div className="flip-card-back">
+              <p
+                style={{
+                  paddingLeft: "12px",
+                  paddingRight: "12px",
+                  textAlign: "justify",
+                }}
+              >
+                We believe that trust, tolerance, and self-awarness are three
+                essential components to building the synergy, also known as
+                similar energies, to cultivate a community where we collectively
+                achieve individual and team goals as a organization.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
